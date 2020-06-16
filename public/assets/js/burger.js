5f0d2aef81e95c28aec49d30fae79095c345c1f5
Haven <stackandslash@gmail.com>
2020-06-15 20:24:16 -0700

    Functionish, but the SQL syntax is off

---
+++ public/assets/js/burger.js
@@ -0,0 +1,39 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+        console.log("Eating Burger");
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: true //This sets Devoured to true.
+      }).then(
+        function() {
+          console.log("Eaten!");
+          // Reload the page
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      event.preventDefault();
+      console.log("Prepping Burger");
+      var newBurger = {
+        name: $("#br").val().trim(),
+        devoured: false
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers/", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("Burger Ready!");
+          location.reload();
+        }
+      );
+    });
+  });
+  
